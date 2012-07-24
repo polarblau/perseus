622b790f202c33c31e64c753ec4ac0c583f4f3d3
Polarblau <polarblau@gmail.com>
2012-07-24 15:12:36 +0300

    Cleaning-up test folder.

--- Rakefile
+++ Rakefile
@@ -3,7 +3,7 @@ require 'rake/testtask'
 
 Rake::TestTask.new do |t|
   t.libs << 'lib'
-  t.test_files = FileList['test/**/*_{test,spec}.rb']
+  t.test_files = FileList['spec/**/*_spec.rb']
   t.verbose = false
 end
 
--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -1,9 +1,18 @@
+lib_dir = File.dirname(__FILE__) + '/../lib'
+
 require 'bundler/setup'
 require 'bundler/setup'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'minitest/spec'
 require 'sass'
+
+$:.unshift lib_dir unless $:.include?(lib_dir)
+
+require 'perseus'
+require 'perseus/parser'
+require 'perseus/compiler'
+require 'perseus/selector'
 
 def fixture(file, type = :sass)
   File.read fixture_path(file, type)
