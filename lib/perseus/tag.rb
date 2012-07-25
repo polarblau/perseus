56db7ee9baac553b444339dfdbad3aea34ef5ffd
Polarblau <polarblau@gmail.com>
2012-07-25 18:32:07 +0300

    Tag rendering.

--- lib/perseus/tag.rb
+++ lib/perseus/tag.rb
@@ -1,13 +1,41 @@
 module Perseus
   class Tag
 
+    SELF_CLOSING_TAGS = %w(img br hr)
+
     # selector must be an instance of Perseus::Selector
     def initialize(selector)
       @selector = selector
     end
 
     def render
-      ''
+      html = "<#{name}#{attributes}"
+      if self_closing?
+        html << " />\n"
+        # TODO: check for children and throw execption?
+      else
+        if @selector.children.any?
+          html << ">\n"
+          html << @selector.children.map(&:to_html).join('\n')
+        else
+          html << ">"
+          # TODO: content
+        end
+        html << "</#{name}>\n"
+      end
+      html
+    end
+
+    def self_closing?
+      SELF_CLOSING_TAGS.include? name
+    end
+
+    def name
+      @selector.tag
+    end
+
+    def attributes
+      @selector.attributes.inject('') {|m, p| m << " #{p[0]}=\"#{p[1]}\""}
     end
 
   end
--- spec/unit/tag_spec.rb
+++ spec/unit/tag_spec.rb
@@ -3,8 +3,12 @@ require File.dirname(__FILE__) + '/../test_helper'
 describe Perseus::Tag do
 
   before :each do
-    @selector = Perseus::Selector.new('#foo')
-    @tag      = Perseus::Tag(@selector)
+    @selector = MiniTest::Mock.new
+    @tag      = Perseus::Tag.new(@selector)
+    # defaults
+    @selector.expect :tag, 'div'
+    @selector.expect :attributes, {}
+    @selector.expect :children, []
   end
 
   after :each do
@@ -13,6 +17,68 @@ describe Perseus::Tag do
   end
 
   describe '#render' do
+
+    it 'should render a div tag' do
+      @tag.render.must_equal "<div></div>\n"
+    end
+
+    it 'should render a div tag with attributes' do
+      @selector.expect :attributes, { 'class' => 'foo' }
+      @tag.render.must_equal "<div class=\"foo\"></div>\n"
+    end
+
+    it 'should render a div tag with child element' do
+      # TODO: mock child_div?
+      child_div = Perseus::Selector.new('div')
+      @selector.expect :children, [child_div]
+      @tag.render.must_equal "<div>\n<div></div>\n</div>\n"
+    end
+
+  end
+
+  describe '#name' do
+
+    it 'should return the tag name' do
+      @selector.expect :tag, 'img'
+      @tag.name.must_equal 'img'
+    end
+
+  end
+
+  describe '#self_closing?' do
+
+    it 'should return true for img, br or hr tags' do
+      %w(img br hr).each do |tag|
+        @selector.expect :tag, tag
+        @tag.self_closing?.must_equal true
+      end
+    end
+
+    it 'should return false for regular tags' do
+      @selector.expect :tag, 'div'
+      @tag.self_closing?.must_equal false
+    end
+
+  end
+
+
+  describe '#attributes' do
+
+    it 'returns an empty string if no attributes are defined' do
+      @selector.expect :attributes, {}
+      @tag.attributes.must_be_empty
+    end
+
+    it 'returns a formatted string for a key:value pair' do
+      @selector.expect :attributes, { 'foo' => 'bar' }
+      @tag.attributes.must_equal ' foo="bar"'
+    end
+
+    it 'returns a string, starting with whitespace' do
+      @selector.expect :attributes, { 'foo' => 'bar' }
+      @tag.attributes[0].must_equal ' '
+    end
+
 
   end
 
