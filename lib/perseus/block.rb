d9c6c094c4632113fc4b3d98bdb8e87ef16f60a2
Polarblau <polarblau@gmail.com>
2013-12-29 23:17:31 +0200

    Updating block.

--- lib/perseus/block.rb
+++ lib/perseus/block.rb
@@ -1,100 +1,35 @@
 module Perseus
-
   class Block
     include StructWithDefaults
-    
-    defaults :name        => '',
-             :description => '',
-             :selector    => '',
-             :mixin       => '',
-             :placeholder => '',
-             :states      => [],
-             :group       => '',
-             :category    => '',
-             :extends     => [],
-             :includes    => [],
-             :arguments   => {},
-             :markup      => nil,
-             :styles      => nil
+    extend Forwardable
 
-    MARKUP_TYPES = %i(html haml slim)
-    STYLES_TYPES = %i(css sass scss)
+    def_delegators :@section, :dependencies, :file_path
+
+    attr_reader :section
+
+    defaults Perseus.configuration.block_defaults
+
+    def initialize(section, attributes = {})
+      @section = section
+      super(attributes)
+    end
 
     def valid?
-      @attributes.has_key?(:name) and not @attributes[:name].empty?
+      # TODO: simplify
+      @attributes.has_key?(:name) && !@attributes[:name].empty?
     end
 
-    # TODO: test that always returns a meaningful data object
-    def markup
-      type, value = markup_source
-      return if type.nil?
-
-      source, line = value[:source], value[:line]
-      
-      @markup ||= OpenStruct.new(
-        :type     => type,
-        :line     => line,
-        :source   => source,
-        :compiled => compile(type, source, line)
-      )
-    end
-
-    # TODO: test that always returns a meaningful data object
-    def styles
-      type, value = styles_source
-      return if type.nil?
-
-      source, line = value[:source], value[:line]
-      source_with_dependencies = import_styles_dependencies(source, type)
-
-      Sass.load_paths << File.dirname(@attributes[:path])
-      @styles ||= OpenStruct.new(
-        :type     => type,
-        :line     => line,
-        :source   => source,
-        # TODO: ensure that this we're only returning what makes sense here!
-        :compiled => compile(type, source_with_dependencies, line)
-      )
-
-      # TODO: NOT THREAD SAFE!
-      Sass.load_paths.pop
-
-      @styles
+    def example
+      @example ||= generate_example
     end
 
   private
 
-    def markup_source
-      @attributes.find {|key, attrs| MARKUP_TYPES.include?(key) }
-    end
-
-    def styles_source
-      @attributes.find {|key, attrs| STYLES_TYPES.include?(key) }
-    end
-
-    def import_styles_dependencies(source, type)
-      dependencies = @attributes[:dependencies].map { |dependency|
-        # TODO: de–couple from type
-        "@import #{dependency}#{ ';' unless type == :sass }\n"
-      }.join('')
-      source.to_s.dup.prepend(dependencies)
-    end
-
-    def compile(type, source, line)
-      return if type.nil? or source.nil?
-
-      begin
-        template = Tilt[type.to_s].new { source }
-        output   = template.render
-      rescue => e
-        line_within_block = e.backtrace.first.split(':').last.to_i
-        line = line + self.line + line_within_block
-        raise BlockParserError.new(
-          "Parsing of #{type} example block failed in #{path}:#{line}.", e
-        )
+    def generate_example
+      if (example = @attributes[:example])
+        example_klass = Perseus.configuration.example_resolver || Example
+        example_klass.send(:new, example, self)
       end
-
-      output
     end
 
   end
--- spec/unit/block_spec.rb
+++ spec/unit/block_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
+require 'pry'
 
 describe Perseus::Block do
-  
-  subject { Perseus::Block.new(:name => 'The block name') }
-  
+
+  let(:section)   { double('Perseus::Section', :file_path    => 'foo/bar.sass',
+                                               :dependencies => [])             }
+  subject(:block) { Perseus::Block.new(section, :name => 'The block name')      }
+
   DEFAULTS = {
     :name        => '',
     :description => '',
@@ -11,35 +14,92 @@ describe Perseus::Block do
     :mixin       => '',
     :placeholder => '',
     :states      => [],
-    :group       => '',
-    :category    => '',
     :extends     => [],
     :includes    => [],
     :arguments   => {},
-    :markup      => nil,
-    :styles      => nil
+    :example     => nil
   }
-  
-  describe '#valid?' do 
+
+  describe '#valid?' do
     it 'returns true if a "name" attribute has been defined' do
-      expect(subject.valid?).to be(true)
+      expect(block.valid?).to be(true)
     end
-    
+
     it 'returns true if a "name" attribute has not been defined' do
-      block = Perseus::Block.new 
+      block = Perseus::Block.new(section)
       expect(block.valid?).to be(false)
     end
   end
-  
-  
+
+  describe '#file_path' do
+    it 'shold delegate #file_path to section' do
+      block.file_path
+      expect(section).to have_received(:file_path)
+    end
+  end
+
+  describe '#dependencies' do
+    it 'shold delegate #dependencies to section' do
+      block.dependencies
+      expect(section).to have_received(:dependencies)
+    end
+  end
+
+  describe '#example' do
+
+    let(:example)   { { :line => 123, :haml => '%h1 foo' }             }
+    subject(:block) { Perseus::Block.new(section, :example => example) }
+
+    it 'creates a new instance of Example' do
+      expect(Perseus::Example).to receive(:new).with(example, block)
+      block.example
+    end
+    
+    it 'should set #example to an instance of the built-in Example class' do
+      expect(block.example).to be_a(Perseus::Example)
+    end
+
+    it 'returns nil if no example has been specified' do
+      block = Perseus::Block.new(section)
+      expect(block.example).to be_nil
+    end
+
+    it 'returns an example if the attributes define one' do
+      block = Perseus::Block.new(section, :example => { :line => 123, :haml => '%h1 foo' })
+      expect(block.example).to_not be_nil
+      # sanity check, may be moved to functional tests
+      expect(block.example.styles).to be_nil
+      expect(block.example.markup).to_not be_nil
+    end
+
+    describe 'custom example resolver' do
+      class CustomExample < Perseus::Example; end
+      
+      before do
+        Perseus.configure do |config|
+          config.example_resolver = CustomExample
+        end
+      end
+
+      it 'should use a custom resolver if defined' do
+        expect(CustomExample).to receive(:new).with(example, block)
+        block.example
+      end
+      
+      it 'should set #example' do
+        expect(block.example).to be_a(CustomExample)
+      end
+    end
+  end
+
   describe 'defaults' do
-    subject { Perseus::Block.new }
-        
+    subject(:block) { Perseus::Block.new(section) }
+
     DEFAULTS.each do |key, value|
       it "should have a default key ':#{key}' with value '#{value}'" do
-        expect(subject).to respond_to(key)
-        expect(subject.attributes).to have_key(key)
-        expect(subject.send(key)).to eq(value)
+        expect(block).to respond_to(key)
+        expect(block.attributes).to have_key(key)
+        expect(block.send(key)).to eq(value)
       end
     end
   end
