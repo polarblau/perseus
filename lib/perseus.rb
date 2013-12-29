b923727da6d95b83f18c7c13b564480c47e58777
Polarblau <polarblau@gmail.com>
2013-12-29 23:14:53 +0200

    Includes.

--- lib/perseus.rb
+++ lib/perseus.rb
@@ -3,8 +3,11 @@ require 'ostruct'
 require 'tilt'
 
 require 'perseus/errors'
+require 'perseus/config'
 require 'perseus/struct_with_defaults'
 require 'perseus/engine'
 require 'perseus/file_parser'
 require 'perseus/block'
 require 'perseus/section'
+require 'perseus/example'
+
