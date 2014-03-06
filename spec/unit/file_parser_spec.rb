16a52fe900465e7c34abfac0ea1028bdb6e7c04d
Polarblau <polarblau@gmail.com>
2014-03-06 15:04:56 +0200

    Clean-up.

--- spec/unit/file_parser_spec.rb
+++ spec/unit/file_parser_spec.rb
@@ -6,29 +6,29 @@ describe Perseus::FileParser do
 
     context 'only section' do
       let(:section) { parse_styles("// @section: The section name") }
-      
+
       it 'should return a section' do
         expect(section).to be_a(Perseus::Section)
       end
-      
+
       it 'should identify the section name' do
         expect(section.name).to eq('The section name')
       end
-      
+
       it 'shuold not find any blocks' do
         expect(section.blocks).to be_empty
       end
     end
-    
+
     context 'section without name' do
       let(:section) { parse_styles("// @dependencies: bar, bat") }
-      
+
       it 'should generate a section name from the file name' do
         expect(section.name).not_to be_empty
         expect(section.name).to match(/^Stylesheet/)
       end
     end
-    
+
     context 'section with valid block' do
       let(:section) do
         parse_styles <<-EOS
@@ -37,15 +37,15 @@ describe Perseus::FileParser do
 // @name: The block name
 EOS
       end
-      
+
       it 'should contain one block' do
         expect(section.blocks).not_to be_empty
         expect(section.blocks.size).to be(1)
         expect(section.blocks.first.name).to eq('The block name')
-      end 
-      
+      end
+
     end
-    
+
     context 'section with invalid block' do
       let(:section) do
         parse_styles <<-EOS
@@ -54,19 +54,19 @@ EOS
 // @foo: bar
 EOS
       end
-      
+
       it 'should not contain any blocks' do
         expect(section.blocks).to be_empty
       end
     end
-    
+
     context 'section with mutiple blocks' do
       let(:section) do
         parse_styles <<-EOS
 // @section: The section name
 
 // @name: First block name
-.foo
+%foo
   color: red
 
 // @name: Second block name
@@ -74,7 +74,7 @@ EOS
   color: green
 EOS
       end
-      
+
       it 'should contain two blocks' do
         expect(section.blocks).not_to be_empty
         expect(section.blocks.size).to be(2)
