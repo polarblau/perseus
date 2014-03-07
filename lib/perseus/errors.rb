bd7ed4fa21f1d9f18715ef524d5ef25b6aa7655b
Polarblau <polarblau@gmail.com>
2014-03-07 10:08:56 +0200

    Better error messages, including original message.

--- lib/perseus/errors.rb
+++ lib/perseus/errors.rb
@@ -1,17 +1,18 @@
 module Perseus
-  
+
   class ParserError < StandardError
     attr_reader :original
 
     def initialize(message, original = nil)
+      message = message + "\n" + original.message if original
       super(message)
       @original = original
     end
   end
-  
+
   class FileParserError < ParserError; end
   class ExampleParserError < ParserError; end
-  
+
   class EngineError < StandardError; end
-  
+
 end
