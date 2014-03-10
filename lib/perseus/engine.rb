26274152e55dd329dd2593dec0c6753d9420421a
Polarblau <polarblau@gmail.com>
2014-03-10 07:17:38 +0200

    Push pool source path onto Sass load paths.

--- lib/perseus/engine.rb
+++ lib/perseus/engine.rb
@@ -9,6 +9,10 @@ module Perseus
       @sections = []
 
       if Dir.exists?(path)
+        # TODO: better way?
+        Sass.load_paths << path
+        Sass.load_paths.uniq!
+
         # TODO: parse dirs recursively and store structured
         @pool = Dir["#{path}/**/*.{css,sass,scss}"].each do |file_path|
           parser = FileParser.new(file_path)
@@ -24,3 +28,19 @@ module Perseus
   end
 
 end
+
+# require 'pstore'
+#
+# store = PStore.new('perseus.pstore')
+#
+# def write_cache(sections)
+#   store.transaction do
+#     store[:sections] = Marshal.dump(sections)
+#   end
+# end
+#
+# def read_cache
+#   store.transaction do
+#     Marshal.load(store[:sections])
+#   end
+# end
